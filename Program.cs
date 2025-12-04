5d6323c365c946ad28bdd976919ef28a486bdf7f
MathiasNaturalich <N00479733@upn.pe>
2025-12-04 08:13:15 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeteriaReservas
+{
+    class Program
+    {
+        const int TURNOS = 2;
+        const int MAX_RESERVAS_POR_TURNO = 20;
+    }
+}
